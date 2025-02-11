d79aa54e35b22434041cdf1980a4fca1d0ebb35d
Brady Burgener <bradylbsee@gmail.com>
2025-02-11 05:33:09 -0700

    adds delete store

--- src/main/java/pet/store/controller/PetStoreController.java
+++ src/main/java/pet/store/controller/PetStoreController.java
@@ -1,9 +1,11 @@
 package pet.store.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -67,5 +69,15 @@ public class PetStoreController {
 	public PetStoreData recievePetStoresById(@PathVariable Long petStoreId){
 		log.info("Retrieving pet store with ID={}", petStoreId);
 		return petStoreService.retrievePetStoreById(petStoreId);
+	}
+	
+	@DeleteMapping("/{petStoreId}")
+	public Map<String, String> deletePetStoreById(@PathVariable Long petStoreId) {
+		log.info("Deleting pet store with ID={} and employees", petStoreId);
+		
+		petStoreService.deletePetStoreById(petStoreId);
+		
+		return Map.of("message", "Pet store with ID=" + petStoreId + " was deleted with it's employees.");
+		
 	}
 }
